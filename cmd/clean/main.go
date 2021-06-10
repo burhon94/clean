5321c0a6d933a7499ac8424e28d59b2c100d8edc
burhon94 <tohirov1994@gmail.com>
2021-06-10 17:29:38 +0500

    feat: server graceStatus

--- cmd/clean/main.go
+++ cmd/clean/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/burhon94/clean/cmd/clean/router"
 )
 
 func main() {
 	var (
-		prefix = "/api"
-		addr   = ":9999"
+		prefix         = "/api"
+		addr           = ":9999"
+		ctx, cancelFun = context.WithCancel(context.Background())
 	)
 
 	newMux := router.InitRouter(prefix)
@@ -20,14 +26,27 @@ func main() {
 		Addr:    addr,
 	}
 
-	defer func() {
+	go func() {
+		quitCh := make(chan os.Signal, 1)
+		signal.Notify(quitCh, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGINT)
+		sig := <-quitCh
+		log.Printf("signal....%v  ✗ ", sig)
+		cancelFun()
 
-		if err2 := recover(); err2 != nil {
-			log.Println(err2)
-			return
+		ctx, cancelFun = context.WithTimeout(ctx, time.Second*10)
+		defer cancelFun()
+		err := newServer.Shutdown(ctx)
+		if err != nil {
+			log.Fatalf("✗ srv.Shutdown, err: %v", err)
 		}
-
-		newServer.ListenAndServe()
-
+		log.Printf("server finished goroutines ✓")
 	}()
+
+	log.Printf("server started at %s ✓ ", newServer.Addr+prefix)
+	err := newServer.ListenAndServe()
+	if err != nil {
+		log.Fatalf("server serve ✗ , err: %v", err)
+	}
+
+	log.Printf("finished main goroutine ✓")
 }
